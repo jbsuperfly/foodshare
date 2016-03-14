3a6896469a19d31b4badbc21cdb11a9d9cb7c2fc
Joshua I Barker <jbsuperfly@hotmail.com>
2016-03-14 09:43:18 -0700

    Third
    Adding basic templates

--- apps/integrator/models.py
+++ apps/integrator/models.py
@@ -1,5 +1,16 @@
 from __future__ import unicode_literals
-
+from django.contrib.auth.models import User
 from django.db import models
-
+from django.apps import apps
+import datetime
 # Create your models here.
+
+Food = apps.get_app_config('foods').models['foods']
+Message = apps.get_app_config('messages').models['messages']
+Review = apps.get_app_config('reviews').models['reviews']
+User = apps.get_app_config('users').models['users']
+
+class (models.Model):
+class (models.Model):
+class (models.Model):
+class (models.Model):
--- foodshare/settings.py
+++ foodshare/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.foods',
+    'apps.integrator',
+    'apps.messages',
+    'apps.reviews',
+    'apps.users',
 ]
 
 MIDDLEWARE_CLASSES = [
